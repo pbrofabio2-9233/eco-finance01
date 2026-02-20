f74700f53085569007c14e5be5dbf647c59a33be
pbrofabio2-9233 <pbrofabio2@gmail.com>
2026-02-20 17:09:15 -0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,7 +1,21 @@
-function salvarDados(dados){
-    localStorage.setItem("ecoFinance", JSON.stringify(dados));
-}
+// js/storage.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
-function carregarDados(){
-    return JSON.parse(localStorage.getItem("ecoFinance")) || [];
-}
+// 🔥 CONFIG DO SEU FIREBASE
+const firebaseConfig = {
+  apiKey: "AIzaSyAPEv9rP_Ek60NrGfMJnEO3dPzFM4j36cI",
+  authDomain: "master-finance-bce36.firebaseapp.com",
+  projectId: "master-finance-bce36",
+  storageBucket: "master-finance-bce36.firebasestorage.app",
+  messagingSenderId: "241893077182",
+  appId: "1:241893077182:web:b8100d0ca968ba90a42eef"
+};
+
+// 🔥 Inicialização
+const app = initializeApp(firebaseConfig);
+
+// 🔥 Serviços
+export const db = getFirestore(app);
+export const auth = getAuth(app);
